c5e0c14b805bf2bb895300fa32bd2892f60c8d00
franciszekostrowski <franekostrowski@protonmail.com>
2021-10-29 22:30:50 +0200

    style: update background color

--- src/components/templates/Layout/NavBar.tsx
+++ src/components/templates/Layout/NavBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="flex min-h-4 p-6">
+    <nav className="flex min-h-4 p-6 bg-gray-50">
       <div className="text-3xl font-bold">
         Breeda<span className="text-green-700">holic</span> 🐶
       </div>
