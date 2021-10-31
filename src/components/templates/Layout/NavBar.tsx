eee070c13225d2253f97a59275001afb84f2ab9a
franciszekostrowski <franekostrowski@protonmail.com>
2021-10-31 12:22:01 +0100

    refactor: remove warnings

--- src/components/templates/Layout/NavBar.tsx
+++ src/components/templates/Layout/NavBar.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const NavBar = () => {
+const NavBar: React.FC = () => {
   return (
-    <nav className="flex p-6 min-h-4">
+    <nav className="flex px-6 py-4 min-h-4">
       <div className="text-3xl font-bold">
         Breeda<span className="text-green-700">holic</span> 🐶
       </div>
