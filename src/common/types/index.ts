5107f195dcfb47fa1daaa66048f55ba4c91422dc
franciszekostrowski <franekostrowski@protonmail.com>
2022-01-28 00:29:47 +0100

    feat: add typs

---
+++ src/common/types/index.ts
@@ -0,0 +1,27 @@
+interface Breed {
+  id: number;
+  name: string;
+  temperament: string;
+  life_span: string;
+  origin?: string;
+  weight: {
+    imperial: string;
+    metrical: string;
+  };
+  height: {
+    imperial: string;
+    metrical: string;
+  };
+  image?: {
+    id: string;
+    url: string;
+    height: number;
+    width: number;
+  };
+  bred_for: string;
+  breed_group: string;
+}
+
+type Breeds = Array<Breed>;
+
+export type { Breeds as BreedsType, Breed as BreedType };
