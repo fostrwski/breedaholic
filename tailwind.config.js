88a2acff225b45673a9cc0e58e66458cbe711f1b
franciszekostrowski <franekostrowski@protonmail.com>
2022-01-28 00:26:34 +0100

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/common/components/**/*.{js,ts,jsx,tsx}",
+    "./src/common/layouts/**/*.{js,ts,jsx,tsx}",
     "./src/modules/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
   plugins: [require("@tailwindcss/forms")],
 };
