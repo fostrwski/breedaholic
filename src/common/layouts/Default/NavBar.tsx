6efdc1526284f579d3eb2c12f73ac73292ce28a1
franciszekostrowski <franekostrowski@protonmail.com>
2022-01-28 00:28:32 +0100

    feat: add DefaultLayout

---
+++ src/common/layouts/Default/NavBar.tsx
@@ -0,0 +1,16 @@
+import Container from "common/components/Container";
+import React from "react";
+
+const NavBar: React.FC = () => {
+  return (
+    <nav className="mb-2">
+      <Container customClasses="py-8">
+        <div className="text-4xl font-bold text-center">
+          Breeda<span className="text-green-700">holic</span> 🐶
+        </div>
+      </Container>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/common/layouts/Default/index.tsx
@@ -0,0 +1,23 @@
+import Container from "common/components/Container";
+import React from "react";
+
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const DefaultLayout: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <NavBar />
+      <Container>
+        <main>{children}</main>
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default DefaultLayout;
