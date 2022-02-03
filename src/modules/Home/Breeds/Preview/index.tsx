1fac479f1a19c4c36049e803e59cda44b2b701a1
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-03 16:04:24 +0100

    feat: add data-cy attributes

--- src/modules/Home/Breeds/Preview/index.tsx
+++ src/modules/Home/Breeds/Preview/index.tsx
@@ -3,18 +3,33 @@ import React from "react";
 import { useFilterBreeds } from "@/common/context/FilterBreeds";
 import { BreedType } from "@/common/types";
 
-import Card from "./Card";
+import BreedCard from "./BreedCard";
 
 const Preview: React.FC = () => {
   const { filteredBreeds } = useFilterBreeds();
 
   return (
     <div className="grid gap-6">
-      {filteredBreeds.map((breed: BreedType) => (
-        <Card key={breed.id} breed={breed} />
-      ))}
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      {filteredBreeds.length === 0 && <div>We couldn't find any breeds :(</div>}
+      {filteredBreeds.map((breed: BreedType) => {
+        return (
+          <BreedCard
+            key={breed.id}
+            breed={breed}
+            data-cy={`${breed.name
+              .toLowerCase()
+              .split(" ")
+              .join("-")}-preview-card`}
+          />
+        );
+      })}
+      {filteredBreeds.length === 0 && (
+        <div className="text-center text-lg">
+          <span data-cy="breeds-not-found-message">
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            We couldn't find any breeds
+          </span>
+        </div>
+      )}
     </div>
   );
 };
