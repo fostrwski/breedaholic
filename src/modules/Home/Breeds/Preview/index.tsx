e77a286e6fee2db0256b4d6de0705fce9e7831c0
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-03 13:15:38 +0100

    fix: no escape error

--- src/modules/Home/Breeds/Preview/index.tsx
+++ src/modules/Home/Breeds/Preview/index.tsx
@@ -1,6 +1,7 @@
-import { useFilterBreeds } from "common/context/FilterBreeds";
-import { BreedType } from "common/types";
 import React from "react";
+
+import { useFilterBreeds } from "@/common/context/FilterBreeds";
+import { BreedType } from "@/common/types";
 
 import Card from "./Card";
 
@@ -12,6 +13,7 @@ const Preview: React.FC = () => {
       {filteredBreeds.map((breed: BreedType) => (
         <Card key={breed.id} breed={breed} />
       ))}
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
       {filteredBreeds.length === 0 && <div>We couldn't find any breeds :(</div>}
     </div>
   );
