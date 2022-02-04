20dee885b1914fedaa3f3725e9806ad389befc94
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-04 20:19:25 +0100

    feat: add redux

---
+++ src/common/redux/store/index.ts
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
