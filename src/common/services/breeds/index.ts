deb2c5f70a90b3843b31eebcbcc1ba2d5cbba6e9
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-07 21:54:49 +0100

    feat: add breeds service

--- src/common/services/breeds/index.ts
+++ src/common/services/breeds/index.ts
@@ -1,25 +1,41 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+import type { AppState } from "@/common/store";
 import type { Breed } from "@/common/types";
 
 interface BreedsState {
-  value: Array<Breed>;
+  data: Array<Breed>;
 }
 
 const initialState: BreedsState = {
-  value: [],
+  data: [],
 };
+
+export const fetchBreeds = createAsyncThunk("breeds/fetchBreeds", async () => {
+  const res = await fetch("https://api.thedogapi.com/v1/breeds");
+  const breeds = await res.json();
+
+  return breeds;
+});
 
 export const breedsSlice = createSlice({
   name: "breeds",
   initialState,
   reducers: {
-    update: (state, action: PayloadAction<Array<Breed>>) => {
-      state.value.concat(action.payload);
+    addBreeds: (state, action: PayloadAction<Array<Breed>>) => {
+      console.log(action.payload);
+      state.data.push(...action.payload);
     },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchBreeds.fulfilled, (state, action) => {
+      state.data = state.data.concat(action.payload);
+    });
   },
 });
 
-export const { update } = breedsSlice.actions;
-export const selectBreeds = (state) => state.breeds.value;
+export const { addBreeds } = breedsSlice.actions;
+
+export const selectBreeds = () => (state: AppState) => state.breeds.data;
+
 export default breedsSlice.reducer;
