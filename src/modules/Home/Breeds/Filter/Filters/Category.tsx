f2a0b2a359972df29d293ac84b6c030b53c2b5d9
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-07 22:00:03 +0100

    chore: remove old variables

--- src/modules/Home/Breeds/Filter/Filters/Category.tsx
+++ src/modules/Home/Breeds/Filter/Filters/Category.tsx
@@ -1,29 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Button from "@/common/components/Button";
-import { useFilterBreeds } from "@/common/context/FilterBreeds";
 
 const Category: React.FC = () => {
-  const { currentCategories, removeCategory, addCategory } = useFilterBreeds();
-  const categories = ["All", "Toy", "Hound", "Herding"];
-
-  const handleClick = (category: string) => {
-    if (currentCategories.includes(category)) {
-      removeCategory(category);
-    } else {
-      addCategory(category);
-    }
-  };
-
   return (
     <div className="flex gap-2 text-sm">
-      {categories.map((category) => (
+      {[].map((category) => (
         <Button
           key={category}
-          onClick={() => handleClick(category)}
-          customClasses={`text-gray-800 bg-gray-100 rounded-xl ${
-            currentCategories.includes(category) ? "bg-gray-200" : ""
-          }`}
+          customClasses={`text-gray-800 bg-gray-100 rounded-xl`}
         >
           {category}
         </Button>
