b73364261e55e020d638883daa27a7abd4bebc8e
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-08 22:17:20 +0100

    feat: implement primitive category based breeds filtering

--- src/modules/Home/Breeds/Filter/Filters/Category.tsx
+++ src/modules/Home/Breeds/Filter/Filters/Category.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 import Button from "@/common/components/Button";
+import { filterBreeds } from "@/common/services/breeds";
 
 const Category: React.FC = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className="flex gap-2 text-sm">
-      {[].map((category) => (
+      {["All", "Toy", "Herding"].map((category) => (
         <Button
           key={category}
           customClasses={`text-gray-800 bg-gray-100 rounded-xl`}
+          onClick={() => dispatch(filterBreeds({ categories: [category] }))}
         >
           {category}
         </Button>
