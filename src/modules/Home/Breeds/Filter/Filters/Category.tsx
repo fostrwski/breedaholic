115b680675f0ea49be1a19f836b5ab059d07e18d
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-09 13:27:49 +0100

    feat: implement new functionalities

--- src/modules/Home/Breeds/Filter/Filters/Category.tsx
+++ src/modules/Home/Breeds/Filter/Filters/Category.tsx
@@ -1,19 +1,39 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Button from "@/common/components/Button";
-import { filterBreeds } from "@/common/services/breeds";
+import { filterBreeds, selectFilters } from "@/common/services/breeds";
 
 const Category: React.FC = () => {
   const dispatch = useDispatch();
+  const breedsFilters = useSelector(selectFilters());
+
+  const selectedCategories = breedsFilters.categories!;
+
+  const toggleCategory = (category: string) => {
+    if (selectedCategories!.includes(category)) {
+    } else if (category === "All") {
+      dispatch(filterBreeds({ categories: [] }));
+    } else {
+      dispatch(filterBreeds({ categories: [...selectedCategories, category] }));
+    }
+  };
 
   return (
     <div className="flex gap-2 text-sm">
       {["All", "Toy", "Herding"].map((category) => (
         <Button
           key={category}
-          customClasses={`text-gray-800 bg-gray-100 rounded-xl`}
-          onClick={() => dispatch(filterBreeds({ categories: [category] }))}
+          customClasses={`text-gray-800 bg-gray-100 rounded-xl ${
+            category === "All" && selectedCategories!.length === 0
+              ? "bg-gray-200"
+              : ""
+          } ${
+            category !== "all" && selectedCategories!.includes(category)
+              ? "bg-gray-200"
+              : ""
+          }`}
+          onClick={() => toggleCategory(category)}
         >
           {category}
         </Button>
--- src/modules/Home/Breeds/Preview/index.tsx
+++ src/modules/Home/Breeds/Preview/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { selectBreeds } from "@/common/services/breeds";
+import { selectFilteredBreeds,selectStatus } from "@/common/services/breeds";
 import type { Breed } from "@/common/types";
 
 import BreedCard from "./BreedCard";
 
 const Preview: React.FC = () => {
-  const breeds = useSelector(selectBreeds());
+  const filteredBreeds = useSelector(selectFilteredBreeds());
+  const breedsStatus = useSelector(selectStatus());
 
   return (
     <div className="grid gap-6">
-      {breeds.filteredBreeds.map((breed: Breed) => {
+      {filteredBreeds.map((breed: Breed) => {
         return (
           <BreedCard
             key={breed.id}
@@ -23,8 +24,8 @@ const Preview: React.FC = () => {
           />
         );
       })}
-      {breeds.status === "loading" && <>Loading...</>}
-      {breeds.status === "idle" && breeds.filteredBreeds.length === 0 && (
+      {breedsStatus === "loading" && <>Loading...</>}
+      {breedsStatus === "idle" && filteredBreeds.length === 0 && (
         <div className="text-center text-lg">
           <span data-cy="breeds-not-found-message">
             {/* eslint-disable-next-line react/no-unescaped-entities */}
