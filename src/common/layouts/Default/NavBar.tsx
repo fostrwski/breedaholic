0d5e7ae0657605f676af6e0280649f6929158ef3
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-09 22:28:22 +0100

    feat: add Link to home page

--- src/common/layouts/Default/NavBar.tsx
+++ src/common/layouts/Default/NavBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 import Container from "@/common/components/Container";
@@ -5,10 +6,12 @@ import Container from "@/common/components/Container";
 const NavBar: React.FC = () => {
   return (
     <nav className="mb-2">
-      <Container customClasses="py-6">
-        <div className="text-4xl font-bold text-center">
-          Breeda<span className="text-green-700">holic</span> 🐶
-        </div>
+      <Container customClasses="py-6 text-center">
+        <Link href="/home" passHref>
+          <a className="text-4xl font-bold">
+            Breeda<span className="text-green-700">holic</span> 🐶
+          </a>
+        </Link>
       </Container>
     </nav>
   );
