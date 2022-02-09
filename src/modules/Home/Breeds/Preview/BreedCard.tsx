b6a65654eb071d6689b1955adcce9f7e5b4e80c5
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-09 22:30:12 +0100

    feat: add Link to breed's page

--- src/modules/Home/Breeds/Preview/BreedCard.tsx
+++ src/modules/Home/Breeds/Preview/BreedCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 import type { Breed } from "@/common/types";
@@ -9,20 +10,22 @@ interface CardProps {
 
 const BreedCard: React.FC<CardProps> = ({ breed, ...props }) => {
   return (
-    <div className="relative" {...props}>
-      <Image
-        src={breed.image!.url}
-        alt={breed.name}
-        width={breed.image!.width}
-        height={breed.image!.height}
-        layout="responsive"
-        className="rounded-xl filter brightness-[48%]"
-      />
-      <div className="absolute bottom-4 px-4 w-full text-white">
-        <div className="text-xl mb-2 font-semibold">{breed.name}</div>
-        <div className="text-lg">{breed.temperament}</div>
-      </div>
-    </div>
+    <Link href={`/breed/${breed.id}`} passHref>
+      <a className="relative hover:opacity-10]" {...props}>
+        <Image
+          src={breed.image!.url}
+          alt={breed.name}
+          width={breed.image!.width}
+          height={breed.image!.height}
+          layout="responsive"
+          className="rounded-xl filter brightness-[48%]"
+        />
+        <div className="absolute bottom-4 px-6 w-full text-white">
+          <div className="text-xl mb-2 font-semibold">{breed.name}</div>
+          <div className="text-lg">{breed.temperament}</div>
+        </div>
+      </a>
+    </Link>
   );
 };
 
