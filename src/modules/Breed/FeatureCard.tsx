06c1ba28064a1902dea8ff94bbb3369663e6c86b
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-10 12:28:58 +0100

    style: make breed feature card more compact

--- src/modules/Breed/FeatureCard.tsx
+++ src/modules/Breed/FeatureCard.tsx
@@ -6,10 +6,10 @@ interface FeatureCardProps {
 }
 
 const FeatureCard: React.FC<FeatureCardProps> = ({ title, content }) => (
-  <article className="rounded-xl bg-gray-100 p-6">
+  <article className="rounded-xl bg-gray-100 px-6 py-5">
     <div className="text-xl font-semibold">{title}</div>
 
-    <div className="mt-2">{content}</div>
+    <div className="mt-1">{content}</div>
   </article>
 );
 
