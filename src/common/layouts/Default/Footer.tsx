e9487a9022a9e97d9d68102d423370f8439df60c
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-10 14:43:54 +0100

    style: add bigger margin to footer

--- src/common/layouts/Default/Footer.tsx
+++ src/common/layouts/Default/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="mt-6 rounded-t-xl bg-gray-50 pb-32 pt-10">
+    <footer className="mt-12 rounded-t-xl bg-gray-50 pb-32 pt-10">
       <div className="text-center font-semibold">
         Copyright &copy; franekostrowski
       </div>
