06e0fb5065597ea7b32356bf81fbdd8383dac517
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-17 15:05:26 +0100

    fix: wikidata image not showing

--- src/common/layouts/Default/Footer.tsx
+++ src/common/layouts/Default/Footer.tsx
@@ -4,11 +4,18 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="mt-12 flex flex-col justify-center rounded-t-xl bg-gray-50 px-8 pb-32 pt-10">
-      <div className="mb-8 text-center font-semibold">
+      <div className="mb-6 text-center font-semibold">
         Copyright &copy; franekostrowski
       </div>
-
-      <Image src="/wikidata.svg" alt="Wikidata" height={40} width={40} />
+      <div className="mx-auto w-3/5">
+        <Image
+          src="/wikidata.png"
+          alt="Wikidata"
+          layout="responsive"
+          height={60}
+          width={180}
+        />
+      </div>
     </footer>
   );
 };
