565423d77340017f0f3e0471a4906d3c771cf1ff
franciszekostrowski <franekostrowski@protonmail.com>
2022-02-17 12:55:01 +0100

    feat: display 'unknown' when certain information about dog breed does not exist

--- src/modules/Breed/FeatureCard.tsx
+++ src/modules/Breed/FeatureCard.tsx
@@ -9,7 +9,7 @@ const FeatureCard: React.FC<FeatureCardProps> = ({ title, content }) => (
   <article className="rounded-xl bg-gray-100 px-6 py-5">
     <div className="text-xl font-semibold">{title}</div>
 
-    <div className="mt-1">{content}</div>
+    <div className="mt-1">{content !== undefined ? content : "Unknown"}</div>
   </article>
 );
 
