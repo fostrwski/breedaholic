87c73e33d9faa71242adc9507100a888c2813f65
franciszekostrowski <franekostrowski@protonmail.com>
2022-04-27 23:52:19 +0200

    fix: old build breaking import

--- src/modules/Index/Breeds/Preview/BreedCard.tsx
+++ src/modules/Index/Breeds/Preview/BreedCard.tsx
@@ -1,8 +1,7 @@
+import type { Breed } from "common/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
-import type { Breed } from "@/common/types";
 
 interface CardProps {
   breed: Breed;
