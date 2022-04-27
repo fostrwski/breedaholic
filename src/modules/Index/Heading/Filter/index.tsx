39a712a1061798ea978e2d722dc72ab061d2647f
franciszekostrowski <franekostrowski@protonmail.com>
2022-04-27 23:46:11 +0200

    chore: remove unused code and imports

--- src/modules/Index/Heading/Filter/index.tsx
+++ src/modules/Index/Heading/Filter/index.tsx
@@ -1,9 +1,7 @@
-import { Dialog, Disclosure } from "@headlessui/react";
+import { Disclosure, Popover } from "@headlessui/react";
 import { AdjustmentsIcon } from "@heroicons/react/outline";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
-import Button from "common/components/Button";
-import Container from "common/components/Container";
-import React, { useState } from "react";
+import React from "react";
 
 import Category from "./Filters/Category";
 import Size from "./Filters/Size";
@@ -20,73 +18,46 @@ const filters = [
 ];
 
 const Filter: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClose = () => {
-    setIsOpen(false);
-  };
-
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
-
   return (
-    <>
-      <Dialog
-        open={isOpen}
-        onClose={handleClose}
-        className="fixed inset-0 z-10 overflow-y-auto"
-      >
-        <Container customClasses="bg-white shadow-2xl pt-6 pb-8 rounded-b-2xl max-w-full">
-          <Dialog.Title className="mb-8 text-2xl font-bold text-green-800">
-            Filter breeds
-          </Dialog.Title>
-          <>
-            {filters.map((filter, id) => (
-              <div key={filter.title}>
-                <Disclosure>
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-xl sm:focus-visible:text-green-800">
-                        <span className="text-xl font-semibold">
-                          {filter.title}
-                        </span>
-                        {open ? (
-                          <ChevronUpIcon className="h-5 w-5" />
-                        ) : (
-                          <ChevronDownIcon className="h-5 w-5" />
-                        )}
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-4">
-                        {filter.component}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
-                {id !== filters.length - 1 && (
-                  <hr className="my-4 rounded-full" />
-                )}
-              </div>
-            ))}
-          </>
-          <Button
-            customClasses="fixed bottom-4 py-4 text-lg w-1/2 inset-x-0 mx-auto"
-            aria-label="Close filter breeds"
-            onClick={handleClose}
-            data-cy="breeds-filter-dialog-close"
-          >
-            Close
-          </Button>
-        </Container>
-      </Dialog>
-      <Button
-        onClick={handleOpen}
+    <Popover>
+      <Popover.Button
         aria-label="Filter breeds"
         data-cy="breeds-filter-dialog-open"
+        className="rounded-lg p-2"
       >
         <AdjustmentsIcon className="h-6 w-6" />
-      </Button>
-    </>
+      </Popover.Button>
+      <Popover.Panel className="absolute inset-x-6 z-[100] mt-6 rounded-lg bg-gray-50 p-6 lg:inset-x-28">
+        <>
+          {filters.map((filter, id) => (
+            <div key={filter.title}>
+              <Disclosure>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="flex w-full items-center justify-between rounded-xl">
+                      <span className="text-xl font-semibold">
+                        {filter.title}
+                      </span>
+                      {open ? (
+                        <ChevronUpIcon className="h-5 w-5" />
+                      ) : (
+                        <ChevronDownIcon className="h-5 w-5" />
+                      )}
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="mt-4">
+                      {filter.component}
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+              {id !== filters.length - 1 && (
+                <hr className="my-4 rounded-full" />
+              )}
+            </div>
+          ))}
+        </>
+      </Popover.Panel>
+    </Popover>
   );
 };
 
