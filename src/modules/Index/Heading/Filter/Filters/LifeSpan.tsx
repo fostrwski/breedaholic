bfedc33b2fe383e3e6628b3b4f36fd31b76df611
franciszekostrowski <franekostrowski@protonmail.com>
2022-04-30 09:33:18 +0200

    style: update styling of range input

--- src/modules/Index/Heading/Filter/Filters/LifeSpan.tsx
+++ src/modules/Index/Heading/Filter/Filters/LifeSpan.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Category: React.FC = () => {
+  const [value, setValue] = useState<number>(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(parseInt(e.target.value));
+  };
+
   return (
-    <div className="flex flex-wrap gap-2 text-sm">
-      <input type="range" className="w-full" step="1" />
+    <div className="flex items-center gap-2 text-sm lg:gap-4">
+      <input
+        type="range"
+        className="w-full appearance-none rounded-lg bg-gray-100 text-green-700 focus:cursor-pointer focus-visible:bg-gray-200 focus-visible:outline-none"
+        step="1"
+        min="0"
+        max="12"
+        value={value}
+        onChange={handleChange}
+      />
+
+      <p>
+        <b>{value}</b>
+      </p>
     </div>
   );
 };
