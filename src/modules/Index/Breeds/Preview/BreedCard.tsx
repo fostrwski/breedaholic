cd3ec282c68f42e73d6443f5371b77a58a805c42
franciszekostrowski <franekostrowski@protonmail.com>
2022-05-23 18:49:17 +0200

    feat: show breed groups in pills

--- src/modules/Index/Breeds/Preview/BreedCard.tsx
+++ src/modules/Index/Breeds/Preview/BreedCard.tsx
@@ -9,6 +9,25 @@ interface CardProps {
 }
 
 const BreedCard: React.FC<CardProps> = ({ breed, customClasses, ...props }) => {
+  const generateEmoji = (breedGroup: string) => {
+    switch (breedGroup) {
+      case "Toy":
+        return "🧸";
+      case "Hound":
+        return "🦆";
+      case "Working":
+        return "💼";
+      case "Herding":
+        return "🐑";
+      case "Sporting":
+        return "🎯";
+      case "Terrier":
+        return "🦊";
+      default:
+        return "🐶";
+    }
+  };
+
   return (
     <Link href={`/breed/${breed.id}`}>
       <a
@@ -20,8 +39,14 @@ const BreedCard: React.FC<CardProps> = ({ breed, customClasses, ...props }) => {
           alt={breed.name}
           layout="fill"
           objectFit="cover"
-          className="rounded-lg brightness-[.6]"
+          className="rounded-lg brightness-[.54]"
         />
+        {breed.breed_group && (
+          <div className="absolute top-4 left-4 flex items-center gap-2 rounded-full bg-green-50/30 p-2 px-4 text-sm font-semibold text-white md:text-base">
+            <span>{generateEmoji(breed.breed_group)}</span>
+            <span>{breed.breed_group} dog</span>
+          </div>
+        )}
         <div className="absolute bottom-4 px-6 text-white">
           <p className="mb-2 text-xl font-semibold">{breed.name}</p>
           <p className="text-lg">{breed.temperament}</p>
