684dc74699a1c7e8ed849b6d168e7c6d46c6ca44
franciszekostrowski <franekostrowski@protonmail.com>
2022-06-07 11:10:04 +0200

    refactor: update imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import "@fontsource/noto-sans/400.css";
 import "@fontsource/noto-sans/500.css";
 
 import { fetchBreeds } from "common/redux/breeds";
-import type { AppStore } from "common/store";
-import { wrapper } from "common/store";
+import type { AppStore } from "common/redux/store";
+import { wrapper } from "common/redux/store";
 import type { AppProps } from "next/app";
 import App from "next/app";
 import React from "react";
