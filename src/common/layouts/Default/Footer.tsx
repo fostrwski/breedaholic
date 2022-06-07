36b5373d351461ad562e458849a7aa194ee97a19
franciszekostrowski <franekostrowski@protonmail.com>
2022-06-07 12:27:30 +0200

    style: update the looks of footer

--- src/common/layouts/Default/Footer.tsx
+++ src/common/layouts/Default/Footer.tsx
@@ -1,21 +1,39 @@
+import Container from "common/components/Container";
 import Image from "next/image";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="mt-12 flex flex-col justify-center rounded-t-xl bg-gray-50 px-8 pb-32 pt-10">
-      <div className="mb-6 text-center font-semibold">
-        Copyright &copy; franekostrowski
-      </div>
-      <div className="mx-auto w-[180px]">
-        <Image
-          src="/wikidata.png"
-          alt="Wikidata"
-          layout="responsive"
-          height={60}
-          width={180}
-        />
-      </div>
+    <footer className="mt-12 rounded-t-xl bg-gray-50">
+      <Container customClasses="py-8 flex flex-col md:flex-row gap-8 md:justify-between items-center">
+        <p className="flex items-center gap-2 text-lg font-semibold text-gray-500">
+          Created by
+          <a
+            href="https://github.com/franekostrowski"
+            target="blank"
+            className="flex items-center gap-1"
+          >
+            <svg
+              viewBox="0 0 16 16"
+              className="h-5 w-5"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+            </svg>
+            franekostrowski
+          </a>
+        </p>
+        <div className="w-[120px]">
+          <Image
+            src="/logos/wikidata.png"
+            alt="Wikidata"
+            layout="responsive"
+            height={60}
+            width={180}
+          />
+        </div>
+      </Container>
     </footer>
   );
 };
