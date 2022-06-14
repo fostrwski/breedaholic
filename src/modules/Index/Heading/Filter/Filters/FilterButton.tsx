969a343569ad91a0fe30fd6c3d10d6d4b60aa506
franciszekostrowski <franekostrowski@protonmail.com>
2022-06-14 20:38:22 +0200

    fix: selected filters are now highlighted

--- src/modules/Index/Heading/Filter/Filters/FilterButton.tsx
+++ src/modules/Index/Heading/Filter/Filters/FilterButton.tsx
@@ -14,13 +14,7 @@ const FilterButton: React.FC<FilterButtonProps> = ({
   return (
     <Button
       {...props}
-      customClasses={`text-gray-800 bg-gray-100 ${
-        filter === "All" && selectedFilters!.length === 0 ? "bg-gray-200" : ""
-      } ${
-        filter !== "all" && selectedFilters!.includes(filter)
-          ? "bg-gray-200"
-          : ""
-      }`}
+      customClasses={`text-gray-800 ${((filter === "All" && selectedFilters.length === 0) || selectedFilters.includes(filter)) ? "bg-gray-200" : "bg-gray-100" }`}
     >
       {filter}
     </Button>
