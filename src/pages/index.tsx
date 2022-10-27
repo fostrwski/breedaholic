89c2e257d9731418b7aa2e5779b57d324f0c9d47
franciszekostrowski <franekostrowski@protonmail.com>
2022-10-28 00:10:04 +0200

    style: increase top margin to create bigger gap on medium sized devices and bigger

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,16 @@ import React from "react";
 const Index: NextPage = () => {
   return (
     <>
-      <SEO title="Strona główna" description="Breedaholic is a web application that helps you find the dog breed you're looking for!" />
+      <SEO
+        title="Strona główna"
+        description="Breedaholic is a web application that helps you find the dog breed you're looking for!"
+      />
 
       <DefaultLayout>
         <section>
           <Heading />
         </section>
-        <section className="mt-6">
+        <section className="mt-6 md:mt-8">
           <Breeds />
         </section>
       </DefaultLayout>
