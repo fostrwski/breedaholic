f6f22d50c893d367ee3a888b1364a3481f13f98c
franciszekostrowski <franekostrowski@protonmail.com>
2022-11-04 20:15:04 +0100

    feat: add link to github's repo

--- src/common/layouts/Default/NavBar.tsx
+++ src/common/layouts/Default/NavBar.tsx
@@ -1,16 +1,25 @@
 import Container from "common/components/Container";
 import Link from "next/link";
 import React from "react";
+import NextImage from "next/image";
 
 const NavBar: React.FC = () => {
   return (
     <nav className="mb-2">
-      <Container customClasses="py-6 text-center">
+      <Container customClasses="py-6 text-center flex items-baseline gap-4">
         <Link href="/">
-          <a className="text-4xl font-bold">
+          <a className="flex-1 text-4xl font-bold">
             Breeda<span className="text-green-700">holic</span> 🐶
           </a>
         </Link>
+
+        <a
+          href="https://github.com/franekostrowski/breedaholic"
+          target="blank"
+          aria-label="Go to Breedaholic's repository on Github"
+        >
+          <NextImage src="/logos/github.svg" width={24} height={24} />
+        </a>
       </Container>
     </nav>
   );
