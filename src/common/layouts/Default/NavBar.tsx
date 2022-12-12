30be696e867701ff71d3322fb6577176849e9e2a
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-12 17:41:10 +0100

    style: change items alignment

--- src/common/layouts/Default/NavBar.tsx
+++ src/common/layouts/Default/NavBar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const NavBar: React.FC = () => {
   return (
     <nav className="mb-2">
-      <Container customClasses="py-6 text-center flex gap-2 sm:gap-0 items-baseline">
+      <Container customClasses="py-6 text-center flex gap-2 sm:gap-0 items-center">
         <Link href="/">
           <a className="flex-1 text-4xl font-bold">
             <span className="text-green-700">Breed</span>aholic 🐶
