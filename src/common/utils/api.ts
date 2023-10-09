140ff82dea45ccdc0f0741797e72aa779746cf57
franciszekostrowski <franekostrowski@protonmail.com>
2023-10-10 00:39:34 +0200

    feat: add breed image url

--- src/common/utils/api.ts
+++ src/common/utils/api.ts
@@ -1,5 +1,7 @@
 const GET_BREEDS_URL = "https://api.thedogapi.com/v1/breeds";
 
+const BREED_IMAGE_URL = "https://cdn2.thedogapi.com/images";
+
 const WIKIPEDIA_URL = "https://en.wikipedia.org/w/api.php";
 
-export { GET_BREEDS_URL, WIKIPEDIA_URL };
+export { BREED_IMAGE_URL, GET_BREEDS_URL, WIKIPEDIA_URL };
